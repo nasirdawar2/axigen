c4eed69655167c17e1441fad1ce341f26e814e22
nasirdawar2 <nasirdawar@gmail.com>
2025-08-22 11:40:05 +0300

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 // MySQL database configuration
 const db = mysql.createConnection({
-host: process.env.DB_HOST,
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
